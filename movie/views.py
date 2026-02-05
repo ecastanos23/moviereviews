a8cd60c361c53896d65654808432c687d0d2fb25
Emmanuel Castaño Sepúlveda <ecastanos@eafit.edu.co>
2026-02-05 12:13:31 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     #return HttpResponse('<h1>Welcome to the Movie Reviews Home Page</h1>')
-    return render(request, 'home.htm', {'name': 'Chun-Li'})
+    return render(request, 'home.htm', {'name': 'Emmanuel'})
 
 def about(request):
     return HttpResponse('<h1>Esta es la página About</h1>')
