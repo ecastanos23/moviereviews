6118102e98a26d15093f3ed02d5b6162077652da
Emmanuel Castaño Sepúlveda <ecastanos@eafit.edu.co>
2026-02-06 09:56:17 -0500

    Agregando modelos de peliculas y plantillas base

--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 # Create your views here.
 def home(request):
     #return HttpResponse('<h1>Welcome to the Movie Reviews Home Page</h1>')
-    return render(request, 'home.htm', {'name': 'Emmanuel'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
+
+def nombre(request):
+    return HttpResponse('<h1>Mi nombre es Emmanuel</h1>')
 
 def about(request):
-    return HttpResponse('<h1>Esta es la página About</h1>')
+    #return HttpResponse('<h1>Esta es la página About</h1>')
+    return render(request, 'about.html')
+
+ 
